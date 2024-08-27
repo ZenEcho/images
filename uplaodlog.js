eae202360640e27e1a6530940f37bb790a0689f2
Allen <56901101+ZenEcho@users.noreply.github.com>
2024-08-27 13:07:41 +0800

    UploadDate:2024年8月27日13时7分48秒

---
+++ uplaodlog.js
@@ -0,0 +1,6 @@
+import '../assets/tailwind.css'
+import { createApp } from 'vue'
+
+import UplaodLog from '../uplaodlog/uplaodlog.vue'
+const uplaodLog = createApp(UplaodLog)
+uplaodLog.mount('#app');
