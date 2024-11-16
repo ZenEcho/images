6d2bbd49faf7c16f0fd1319f4f665805cdbe221e
Allen <56901101+ZenEcho@users.noreply.github.com>
2024-11-16 22:41:14 +0800

    Delete file

--- uplaodlog.js
+++
@@ -1,6 +0,0 @@
-import '../assets/tailwind.css'
-import { createApp } from 'vue'
-
-import UplaodLog from '../uplaodlog/uplaodlog.vue'
-const uplaodLog = createApp(UplaodLog)
-uplaodLog.mount('#app');
